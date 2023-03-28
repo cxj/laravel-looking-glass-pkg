3b77d86e67c8e20255601169c2e5e154042995f4
Chris Johnson <cxjohnson@gmail.com>
2023-03-27 22:45:02 -0500

    Add webhook and health routes.

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,14 @@
 
 use Cxj\LookingGlass\Http\Controllers\HealthDashboard;
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
+
+
+Route::group(['prefix' => 'api', 'middleware' => 'api'], function() {
+    /** @phpstan-ignore-next-line  Method added via macro in Spatie library */
+    Route::webhooks('/webhook');
+});
 
 Route::get('/lg-dash', HealthDashboard::class)->name('looking-glass-dashboard');
+Route::get('/health', HealthCheckResultsController::class);
+
